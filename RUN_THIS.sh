ccd628f5342f5531382e669772af5818eeadc599
Ajay Tripathi <chausies@berkeley.edu>
2020-05-25 15:38:45 -0700

    Doing only basic compilation for YCM

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -2,4 +2,4 @@ cd ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 git submodule update --init --recursive --remote --merge
 cd bundle/YouCompleteMe/
-python3 install.py --clangd-completer --cs-completer --go-completer --ts-completer --java-completer
+sudo python3 install.py
