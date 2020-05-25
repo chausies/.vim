d7b731ec4d673edf60289339e45160d61b4732b6
Ajay Tripathi <chausies@berkeley.edu>
2020-05-25 15:48:55 -0700

    Added extra submodule update as test

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -1,5 +1,7 @@
 cd ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
+git submodule update --init --recursive
 git submodule update --init --recursive --remote
+git submodule update --init --recursive
 cd bundle/YouCompleteMe/
 sudo python3 install.py
