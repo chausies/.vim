fe6c39085994675aac9c3b3918c3269b8f525cbd
Ajay Tripathi <chausies@berkeley.edu>
2020-05-25 13:39:23 -0700

    Fixed submodule recursive update.

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -1,5 +1,5 @@
 cd ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
-git submodule update --init --recursive
+git submodule update --init --recursive --remote --merge
 cd bundle/YouCompleteMe/
 python3 install.py --all
