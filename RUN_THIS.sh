2019c342d4fb7efff063bb5b551e812d87081484
Ajay Tripathi <chausies@berkeley.edu>
2020-05-25 15:12:04 -0700

    Fixed YCM compiling

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -2,4 +2,4 @@ cd ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 git submodule update --init --recursive --remote --merge
 cd bundle/YouCompleteMe/
-python3 install.py --all
+python3 install.py --clangd-completer --all
