ed48ee6fa6ca053bc7f7cb662273a69b39450c7e
Ajay Tripathi <chausies@berkeley.edu>
2020-05-25 15:39:26 -0700

    Removed superfluous merge flag

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -1,5 +1,5 @@
 cd ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
-git submodule update --init --recursive --remote --merge
+git submodule update --init --recursive --remote
 cd bundle/YouCompleteMe/
 sudo python3 install.py
