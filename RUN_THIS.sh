03b8fe318f3c848dee08e8743558df5c0584790e
chausies <chausies@berkeley.edu>
2020-05-25 18:02:40 -0700

    Removed rust-completer from YCM compilation

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -3,4 +3,6 @@ ln -s ~/.vim/.vimrc ~/.vimrc
 git submodule update --init --recursive --remote
 sh update_plugins.sh
 cd bundle/YouCompleteMe/
-sudo python3 install.py --all
+# No rust-completer
+python3 install.py --clangd-completer --cs-completer --go-completer --ts-completer --java-completer
+# python3 install.py --all
