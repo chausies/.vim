bad2e0abe00a9ab0bdd3e97208f600413c3e8507
chausies <chausies7@gmail.com>
2026-02-27 01:33:51 -0800

    Switched to --all for YCM

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -3,6 +3,6 @@ ln -s ~/.vim/.vimrc ~/.vimrc
 git submodule update --init bundle/Vundle.vim
 sh update_plugins.sh
 cd bundle/YouCompleteMe/
-# No rust-completer
-python3 install.py --clangd-completer --cs-completer --go-completer --ts-completer --java-completer
-# python3 install.py --all
+# # No rust-completer
+# python3 install.py --clangd-completer --cs-completer --go-completer --ts-completer --java-completer
+python3 install.py --all
